f02aa551d96838bfdc97c32a650ac3e4c19f80e6
Tadeo Casiraghi <tadeo.casiraghi@gmail.com>
2022-03-23 19:06:29 -0300

    Calibrate servos and comunicate with python
    calibrated servo motor to get best sound and created code to communicate with the arduino nano through python

--- drum_machine/teclas.cpp
+++ drum_machine/teclas.cpp
@@ -13,30 +13,32 @@ void Teclas::initialize(){
   pca2.begin();
   pca2.setPWMFreq(50);  // Analog servos run at ~50 Hz updates
   
-  for(int i = 0; i< NTECLAS; i++){
-      delay(100);
-      play_key(i);
-      delay_down[i] = 0;
-      delay(100);
-      key_up(i);
-  }
+//  for(int i = 0; i< NTECLAS; i++){
+//      delay(100);
+//      play_key(i);
+//      delay_down[i] = 0;
+//  }
   
 }
 
 void Teclas::play_key(int key){
-  if (key < 9){
-    pca1.writeMicroseconds(key, angle2PWM(30, key));
-  } else {
-    pca2.writeMicroseconds(key - 9, angle2PWM(30, key));
+  if (delay_down[key] == 0){
+    if (key < 9){
+      pca1.writeMicroseconds(key, angle2PWM(40 + low_note[key], key));
+    } else {
+      pca2.writeMicroseconds(key - 9, angle2PWM(40 + low_note[key], key));
+    }
+    delay_down[key] = millis() + delay1;
   }
-  delay_down[key] = millis() + delay1;
+  
 }
 
 void Teclas::key_up(int key){
+  delay_down[key] = 0;
   if (key < 9){
-    pca1.writeMicroseconds(key, angle2PWM(50, key));
+    pca1.writeMicroseconds(key, angle2PWM(57, key));
   } else {
-    pca2.writeMicroseconds(key - 9, angle2PWM(50, key));
+    pca2.writeMicroseconds(key - 9, angle2PWM(57, key));
   }
 }
 
@@ -47,13 +49,14 @@ void Teclas::play_loop(){
   for(int i = 0; i<NTECLAS; i++){
       if ((delay_down[i] != 0) and (millis() > delay_down[i])){
         key_up(i);
-        delay_down[i] = 0;
       }
     }
 }
 
 int Teclas::angle2PWM(double x, int i) { /* function angle2PWM */
                                  ////Convert joint angle into pwm command value
+  x += CORECTOR[i];
+  
   int imp = (x - MIN_ANG[i]) * (MAX_IMP[i] - MIN_IMP[i]) / (MAX_ANG[i] - MIN_ANG[i]) + MIN_IMP[i];
   imp = max(imp, MIN_IMP[i]);
   imp = min(imp, MAX_IMP[i]);
