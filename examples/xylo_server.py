377d62e057e713170efb7ea956a9fb9cfd36bded
Alvaro <alvgaona@gmail.com>
2022-06-22 15:58:08 -0300

    squash

--- examples/xylo_server.py
+++ examples/xylo_server.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-from xylophone.server.server import MockXyloServer
+from xylophone.server.server import XyloServer
 
 if __name__ == '__main__':
     """
-    In this script we show how to start a XyloServer that simulates a xylophone.
-    This starts a TCP server that will listen for messages sent with the
-    XyloClient
+    In this script we show how to start a server to communicate with the real
+    xylophone.
+    This starts a TCP server that will listen for messages sent with the XyloClient
     """
-    server = MockXyloServer(host='localhost', port=8080)
+    server = XyloServer(host='localhost', port=8080)
     server.start()
 
